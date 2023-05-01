35da8f8cd48a42550461ae0b758ec22d2db34872
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-01 11:49:52 +0330

    Adding a Component for Add Customer

---
+++ src/app/modules/customer/add-customer/add-customer.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Customer } from 'src/app/models/customer.model';
+import { CustomerService } from 'src/app/services/customer.service';
+import { RouterModule, Routes } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CustomerValidator } from './add-customer-validator.service';
+import { ValidationErrors } from 'fluentvalidation-ts/dist/ValidationErrors';
+
+@Component({
+  selector: 'app-add-customer',
+  templateUrl: './add-customer.component.html',
+  styleUrls: ['./add-customer.component.css']
+})
+export class AddCustomerComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private customerService: CustomerService,
+    private _snackBar: MatSnackBar,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      email: ['']
+    });
+  }
+
+  validator:CustomerValidator = new CustomerValidator();
+  validationResult: ValidationErrors<Customer> = {};
+  //a:CustomerValidator;
+  onSubmit(): void {
+    //if (this.form.valid) 
+    {
+      const customer: Customer = {
+        id: this.form.value.id,
+        firstName: this.form.value.firstName,
+        lastName: this.form.value.lastName,
+        email: this.form.value.email
+      };
+
+      this.validationResult = this.validator.validate(customer);
+      if (Object.keys(this.validationResult).length <= 0) {
+        this.customerService.addCustomer(customer).subscribe(() => {
+          this.form.reset();
+
+        });
+        this._snackBar.open('Customer added successfully!', 'Dismiss', {
+          duration: 2000 // 15 seconds
+        });
+        this.router.navigate(['../'], { relativeTo: this.route });
+      }
+      else {
+        this._snackBar.open('Please Solve Error Messages!', 'Dismiss', {
+          duration: 2000 // 15 seconds
+        });
+      }
+    }
+  }
+}
--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { AngularMaterialModule } from 'src/app/angular-material.module';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
 
 
 
 @NgModule({
   declarations: [
-    CustomerListComponent
+    CustomerListComponent,
+    AddCustomerComponent
   ],
   imports: [
     CommonModule,
