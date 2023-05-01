dccadaf07b0be0c35e88c0565bbcd2cf6b25d86d
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-01 10:30:16 +0330

    1-install @angular/cdk --save 2-@angular/material 3- create a module for angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerModule } from './modules/customer/customer.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { CustomerModule } from './modules/customer/customer.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CustomerModule
+    CustomerModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
