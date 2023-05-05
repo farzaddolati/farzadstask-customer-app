d92625d1d749dcd574b95f5cdca09d5800688edf
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-05 22:41:04 +0330

    modifying app routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerListComponent } from './modules/customer/customer-list/customer-list.component';
+import { CityListComponent } from './modules/city/city-list/city-list.component';
 
 const routes: Routes = [
-  { path: '', component: CustomerListComponent }
+  { path: '', component: CustomerListComponent },
+  { path: 'customers', component: CustomerListComponent },
+  { path: 'cities', component: CityListComponent}
 ];
 
 @NgModule({
