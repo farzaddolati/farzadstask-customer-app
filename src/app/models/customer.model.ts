f7bb4aa3a15dfc865116fe707bf703a330abf222
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-05 18:13:29 +0330

    Add City Model

--- src/app/models/customer.model.ts
+++ src/app/models/customer.model.ts
@@ -1,6 +1,12 @@
+
+import { City } from "./city.model";
 export interface Customer{
     id: number;
     firstName: string;
     lastName: string;
     email: string;
+    phoneNumber: string;
+    address: string;
+    cityId:number;
+    city:City;
 }
