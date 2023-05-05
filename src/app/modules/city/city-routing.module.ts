c3d6c5a2584350da9e49e4bde2f06fe7e960d476
Farzad-PC <Farzad-PC@DESKTOP-BLNHSLA>
2023-05-05 19:14:34 +0330

    Creating a routing module for the city

--- src/app/modules/city/city-routing.module.ts
+++ src/app/modules/city/city-routing.module.ts
@@ -1,2 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { CityListComponent } from './city-list/city-list.component';
+import { AddCityComponent } from './add-city/add-city.component';
+import { EditCityComponent } from './edit-city/edit-city.component';
+
+const routes: Routes = [
+    { path: 'cities', component: CityListComponent },
+    { path: 'add-city', component: AddCityComponent },
+    { path: 'edit-city/:id', component: EditCityComponent }
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+  export class CityRoutingModule { }
